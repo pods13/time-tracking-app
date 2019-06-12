40948bcafc2c8c6ef5aaf0688cb0255fafbe131f
pods13 <alta0815@yandex.ru>
2019-06-12 20:00:00 +0300

    Adding state to TimerForm

--- public/app.js
+++ public/app.js
@@ -80,6 +80,11 @@ class EditableTimer extends React.Component {
 
 class TimerForm extends React.Component {
 
+	state = {
+		title: this.props.title || '',
+		project: this.props.project || ''
+	}
+
 	render() {
 		const submitText = this.props.title ? 'Update' : 'Create';
 		return (
@@ -88,11 +93,11 @@ class TimerForm extends React.Component {
 					<div className='ui form'>
 						<div className='field'>
 							<label>Title</label>
-							<input type='text' defaultValue={this.props.title}/>
+							<input type='text' value={this.state.title} onChange={this.handleTitleChange}/>
 						</div>
 						<div className='field'>
 							<label>Project</label>
-							<input type='text' defaultValue={this.props.project} />
+							<input type='text' value={this.state.project} onChange={this.handleProjectChange}/>
 						</div>
 						<div className='ui two bottom attached buttons'>
 							<button className='ui basic blue button'>
@@ -104,6 +109,14 @@ class TimerForm extends React.Component {
 				</div>
 			</div>
 		);
+	}
+
+	handleTitleChange = (e) => {
+		this.setState({ title: e.target.value });
+	}
+
+	handleProjectChange = (e) => {
+		this.setState({ project: e.target.value });
 	}
 }
 
